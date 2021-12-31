869b76fe4102f59f4fc29f532f59d888a078d2be
Priya Sharma <54985329+priyash243@users.noreply.github.com>
2021-12-31 15:03:16 +0530

    Files added

---
+++ website/views.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        
+        
+
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    
+
+    return jsonify({})
